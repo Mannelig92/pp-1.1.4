3a466879fb4b037cdb1ed8488c385017126489d5
Mannelig <mannelig92@gmail.com>
2022-12-14 17:56:06 +0300

    v1.0

---
+++ target/test-classes/test.sql
@@ -0,0 +1,6 @@
+#insert into users (name, age, email) values('John', 21, 'John@mail.com');
+#select name, age from users where id = 1;
+#update users set name = "Steve", age = 55, email = "Steve@mail.com" where id = 2;
+#delete from users where id = 2;
+select * from Users;
+#Это тут для практики и проверки таблицы
